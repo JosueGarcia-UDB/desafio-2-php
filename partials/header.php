21566a553829006f65905489326d55ec754f9f86
EdgarMelendez06 <GM240279@alumno.udb.edu.org.sv>
2025-04-01 19:21:34 -0600

    Estilos

--- partials/header.php
+++ partials/header.php
@@ -1,1 +1,12 @@
-<h1>Hola mundo</h1>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Nómina</title>
+    <link rel="stylesheet" href="/PHPdesafio2/public/css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Sistema de Nómina de Empleados</h1>
+    </header>
